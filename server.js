68aeee773de634aec6d91373f051d4c9b594560e
engmung <122682380+engmung@users.noreply.github.com>
2024-04-13 02:16:27 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
+const WebSocket = require("ws");
 const express = require("express");
+const http = require("http");
 
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+let storedColors = { pmColor: "00FF00", tempColor: "FF0000" };
+
 app.use(express.static("public"));
 
 app.get("/setColor", (req, res) => {
   const { firstColor, secondColor } = req.query;
-  const colorStr = `${firstColor};${secondColor}\n`;
-  console.log("Color set:", colorStr);
-  res.send("Color updated");
+  storedColors = { pmColor: firstColor, tempColor: secondColor };
+  broadcastColors();
+  res.send("Colors updated and broadcasted");
 });
 
-app.listen(3000, () => {
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
+  });
+
+  ws.on("close", () => {
+    console.log("Client has disconnected");
+  });
+});
+
+function broadcastColors() {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(storedColors));
+    }
+  });
+}
+
+server.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
