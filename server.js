fbc9c1052e248a7a53677d84b40baffd23e7ba60
engmung <122682380+engmung@users.noreply.github.com>
2024-04-19 19:59:02 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
-const WebSocket = require("ws");
 const express = require("express");
 const http = require("http");
+const WebSocket = require("ws");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-let storedColors = { pmColor: "00FF00", tempColor: "FF0000" };
+let storedData = {
+  pmColor: "00FF00",
+  tempColor: "FF0000",
+  dustLevel: "40",
+  temperature: "25°C",
+};
 
 app.use(express.static("public"));
 
+// 색상 업데이트 라우트
 app.get("/setColor", (req, res) => {
   const { firstColor, secondColor } = req.query;
-  storedColors = { pmColor: firstColor, tempColor: secondColor };
-  broadcastColors();
-  res.send("Colors updated and broadcasted");
+  storedData.pmColor = firstColor;
+  storedData.tempColor = secondColor;
+  broadcastData();
+  res.send("Colors and data updated and broadcasted");
 });
+
+// 데이터 브로드캐스트 함수
+function broadcastData() {
+  const dataToSend = JSON.stringify(storedData);
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(dataToSend);
+    }
+  });
+}
 
 wss.on("connection", function connection(ws) {
   ws.on("message", function incoming(message) {
@@ -26,14 +45,6 @@ wss.on("connection", function connection(ws) {
     console.log("Client has disconnected");
   });
 });
-
-function broadcastColors() {
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify(storedColors));
-    }
-  });
-}
 
 server.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
